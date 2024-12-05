74610325d60007b9e0bf03a0b143b4498f280704
jordanpg <3819183+jordanpg@users.noreply.github.com>
2024-12-05 00:43:42 -0500

    Feature/pnpm ci (#2)
    * move to pnpm
    
    * add eslint
    
    * wow

--- packages/func-runner/src/main.ts
+++ packages/func-runner/src/main.ts
@@ -1,4 +1,3 @@
 import { bootstrapRunner } from '@nx-extend/gcp-functions/runner';
 
-/* eslint-disable @nx/enforce-module-boundaries */
 bootstrapRunner(new Map([['mtg-api', import('../../mtg-api/src/main')]]));
--- packages/mtg-api/jest.config.ts
+++ packages/mtg-api/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'mtg-api',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.config.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -10,6 +10,6 @@ export default {
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'html', 'mts'],
   coverageDirectory: '../../coverage/packages/mtg-api',
 };
